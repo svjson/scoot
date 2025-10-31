7ff7eff0f707f4a2516bb9e5d7815a15eb0eaa7b
Sven Johansson <sven.johansson@iteam.se>
2025-10-31 12:07:42 +0100

    Inform pyright/lsp about project structure

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((python-mode
+  . ((lsp-pylsp-plugins-pylint-enabled . nil)
+     (lsp-pylsp-plugins-flake8-enabled . nil)
+     (lsp-pylsp-plugins-autopep8-enabled . nil)
+     (python-shell-extra-pythonpaths . ("core" "cli" "server"))
+     (lsp-pyright-extra-paths . ["core" "cli" "server"]))))
+
+;; (("system_test" . ((python-ts-mode
+;;                     (eval . (setq lsp-pyright-extra-paths (list "../core")))))))
