45c51aded8d5e05e2523886074e74971363942c7
Sven Johansson <johansson.sven@gmail.com>
2025-11-27 18:21:54 +0100

    scoot.el: Use condition-case over unwind-protect for scoot-server--running-p

--- scoot.el/scoot-server.el
+++ scoot.el/scoot-server.el
@@ -104,13 +104,17 @@
 
 (defun scoot-server--running-p ()
   "Return non-nil if the Scoot server appears to be running."
-  (let (url-buf)
-    (unwind-protect
-        (when (setq url-buf (url-retrieve-synchronously (format "%s/ping" (scoot-server--base-url)) t t 1))
-          (with-current-buffer url-buf
-            (goto-char (point-min))
-            (re-search-forward "200 OK" nil t)))
-      (when url-buf (kill-buffer url-buf)))))
+  (let (url-buf success)
+    (condition-case _
+        (setq url-buf (url-retrieve-synchronously (format "%s/ping" (scoot-server--base-url)) t t 1))
+      (error
+       (message "Scoot Server is not running")))
+    (when (bufferp url-buf)
+      (setq success (with-current-buffer url-buf
+                      (goto-char (point-min))
+                      (re-search-forward "200 OK" nil t)))
+      (kill-buffer url-buf))
+    success))
 
 (defun scoot-server--venv-operation (op)
   "Resolve venv root, python binary and pip binary and then execute OP."
