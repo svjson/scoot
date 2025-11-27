cda344dd5d0504dbf65572be175de6ae5848d018
Sven Johansson <johansson.sven@gmail.com>
2025-11-27 16:37:07 +0100

    systemtest: Replicate activate venv behavior in start_emacs_daemon.sh

--- system_test/emacs/start_emacs_daemon.sh
+++ system_test/emacs/start_emacs_daemon.sh
@@ -4,6 +4,13 @@ INST=$SCOOT_EMACS_INSTANCE_NAME
 EMD=$SCOOT_TEMP_EMACS_DIR
 CONN=$SCOOT_TEST_CONNECTION
 
+# Activate virtualenv for subprocesses
+if [ -f ".venv/bin/activate" ]; then
+    # Replicate virtualenv activate behavior
+    export PATH="$(pwd)/.venv/bin:$PATH"
+    export VIRTUAL_ENV="$(pwd)/.venv"
+fi
+
 # Start daemon and trim X11 warning on stderr
 emacs --daemon=$INST --init-directory "${EMD}"  2>&1 | grep -v -A 4 -B 1 "Warning: due to a long standing Gtk+ bug"
 
